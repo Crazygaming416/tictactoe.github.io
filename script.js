1f96ad33ada57fdeb8ef0f6e6733a93945d8eaa6
Mohammad-416 <149521106+Mohammad-416@users.noreply.github.com>
2023-12-19 18:30:38 +0530

    added game over functionality

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-window.onload = function()
-{
+window.onload = function () {
     var cells = document.getElementsByClassName("cell");
 
     for (var i = 0; i < cells.length; i++) {
@@ -9,26 +8,83 @@ window.onload = function()
 
 var Player_Turn = false;
 var setter_value;
+var draw = false;
+function checkDraw() {
+    var cells = document.getElementsByClassName("cell");
 
-function Put(obj)
-{
-    if(obj.innerText != "0")
-    {
+    for (var i = 0; i < cells.length; i++) {
+        if (cells[i].innerText != "0") {
+            draw = true;
+        }
+        else {
+            continue;
+        }
+    }
+}
+
+function checkOver() {
+    var count1 = 0;
+    var count2 = 0;
+    for (let j = 0; j < 3; j++) {
+        var row = document.getElementsByClassName("r-"+j);
+        for (var i = 0; i < row.length; i++) {
+            if (row[i].innerText == "1") {
+                count1++;
+            }
+            else if (row[i].innerText == "-1") {
+                count2++;
+            }
+
+            if (count1 == 3) {
+                console.log("player1Won");
+            }
+            else if (count2 == 3) {
+                console.log("player2Won");
+            }
+        }
+        count1=0;
+        count2=0;
+    }
+    for (let j = 0; j < 3; j++) {
+        var row = document.getElementsByClassName("c-"+j);
+        for (var i = 0; i < row.length; i++) {
+            if (row[i].innerText == "1") {
+                count1++;
+            }
+            else if (row[i].innerText == "-1") {
+                count2++;
+            }
+
+            if (count1 == 3) {
+                console.log("player1Won");
+            }
+            else if (count2 == 3) {
+                console.log("player2Won");
+            }
+        }
+        count1=0;
+        count2=0;
+    }
+    checkDraw();
+}
+
+function Put(obj) {
+    if (obj.innerText != "0") {
         return;
     }
 
-    if (Player_Turn==false)
-    {
+    if (Player_Turn == false) {
         setter_value = "-1";
         Player_Turn = true;
     }
-    else
-    {
+    else {
         setter_value = "1";
         Player_Turn = false;
     }
 
     obj.innerText = setter_value;
     console.log(setter_value, obj);
-    
+
+    checkOver();
+
 }
