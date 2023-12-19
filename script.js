c94691c3f525d1e0ddf68481e548effccf9db476
Mohammad-416 <149521106+Mohammad-416@users.noreply.github.com>
2023-12-19 21:32:50 +0530

    fixed draw function and added diagonal gameOver

--- script.js
+++ script.js
@@ -9,39 +9,41 @@ window.onload = function () {
 var Player_Turn = false;
 var setter_value;
 var draw = false;
+var gameOver = false;
 
-function printWinner(count1, count2)
-{
+function printWinner(count1, count2) {
     if (count1 == 3) {
         console.log("player1Won");
+        gameOver = true;
     }
     else if (count2 == 3) {
         console.log("player2Won");
+        gameOver = true;
     }
 }
 
 function checkDraw() {
     var cells = document.getElementsByClassName("cell");
-
+    draw = true;
     for (var i = 0; i < cells.length; i++) {
-        if (cells[i].innerText != "0") {
-            draw = true;
+        if (cells[i].innerText == "0") {
+            draw = false;
         }
-        else {
-            continue;
-        }
+    }
+    if (draw) {
+        gameOver = true;
     }
 }
 
 function checkOver() {
     var count1 = 0;
     var count2 = 0;
-    
+
     var cells = document.getElementsByClassName("cell");
 
 
     for (let j = 0; j < 3; j++) {
-        var row = document.getElementsByClassName("r-"+j);
+        var row = document.getElementsByClassName("r-" + j);
         for (var i = 0; i < row.length; i++) {
             if (row[i].innerText == "1") {
                 count1++;
@@ -52,12 +54,12 @@ function checkOver() {
 
             printWinner(count1, count2);
         }
-        count1=0;
-        count2=0;
+        count1 = 0;
+        count2 = 0;
     }
 
     for (let j = 0; j < 3; j++) {
-        var col = document.getElementsByClassName("c-"+j);
+        var col = document.getElementsByClassName("c-" + j);
         for (var i = 0; i < col.length; i++) {
             if (col[i].innerText == "1") {
                 count1++;
@@ -67,32 +69,31 @@ function checkOver() {
             }
 
             printWinner(count1, count2);
-            
+
         }
-        count1=0;
-        count2=0;
+        count1 = 0;
+        count2 = 0;
     }
 
     let iter = 0;
     for (let j = 0; j < 3; j++) {
         for (var i = 0; i < 3; i++) {
-            if(cells[iter].innerText == "1" && (i==j || row.length-1-i==j))
-            {
+            if (cells[iter].innerText == "1" && (i == j || row.length - 1 - i == j)) {
                 count1++;
             }
-            else if(cells[iter].innerText == "-1" && (i==j || row.length-1-i==j))
-            {
+            else if (cells[iter].innerText == "-1" && (i == j || row.length - 1 - i == j)) {
                 count2++;
             }
             iter++;
-            
+
             printWinner(count1, count2);
         }
     }
     count1 = 0;
     count2 = 0;
 
-    checkDraw();
+    if (!gameOver) { checkDraw(); }
+    else { }
 }
 
 function Put(obj) {
